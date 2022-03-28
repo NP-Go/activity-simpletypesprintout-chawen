6b6118827be281a8347eb369b7b3d1af2db6e956
Wen Low <chawenmushu@gmail.com>
2022-03-28 16:35:40 +0800

    Added type and float

--- typesInGo.go
+++ typesInGo.go
@@ -1,8 +1,15 @@
 package main
 
+import "fmt"
+
 //Insert variables declarations here based on activity
 
 func tellMeTypes() {
+	text := "Account info"
+	credit := 122.22
+	// float
+	fmt.Printf("%T %T", text, credit)
+
 	//insert code here to print out types of variables
 }
 
